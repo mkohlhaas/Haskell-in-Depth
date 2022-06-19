8f86912fb9f5bcaa9d35a2b8c9aa8162e828877d
Hans-J. Schmid <knock@myopendoor.de>
2022-06-19 19:51:11 +0200

    cabalizes ch. 9.

--- ch09/unboxed.hs
+++ ch09/unboxed.hs
@@ -1,27 +1,27 @@
 {-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE UnboxedSums #-}
 
-sum_prod :: Num a => a -> a -> (# a, a #)
-sum_prod a b = (# a + b, a*b #)
+sumProd :: Num a => a -> a -> (# a, a #)
+sumProd a b = (# a + b, a*b #)
 
-test_sum_prod :: IO ()
-test_sum_prod = case sum_prod (5 :: Int) 6 of
+test_sumProd :: IO ()
+test_sumProd = case sumProd (5 :: Int) 6 of
                   (# s, p #) -> print (s + p)
 
-fib_next :: Int -> (Integer, Integer) -> (Integer, Integer)
-fib_next 0 p = p
-fib_next n (a, b) = fib_next (n-1) (b, a+b)
+fibNext :: Int -> (Integer, Integer) -> (Integer, Integer)
+fibNext 0 p = p
+fibNext n (a, b) = fibNext (n-1) (b, a+b)
 
-test_fib_next :: IO ()
-test_fib_next = case fib_next 100 (0, 1) of
+test_fibNext :: IO ()
+test_fibNext = case fibNext 100 (0, 1) of
          (a, _) -> print a
 
-fib_next' :: Int -> (# Integer, Integer #) -> (# Integer, Integer #)
-fib_next' 0 p = p
-fib_next' n (# a, b #) = fib_next' (n-1) (# b, a+b #)
+fibNext' :: Int -> (# Integer, Integer #) -> (# Integer, Integer #)
+fibNext' 0 p = p
+fibNext' n (# a, b #) = fibNext' (n-1) (# b, a+b #)
 
-test_fib_next' :: IO ()
-test_fib_next' = case fib_next' 100 (# 0, 1 #) of
+test_fibNext' :: IO ()
+test_fibNext' = case fibNext' 100 (# 0, 1 #) of
          (# a, _ #) -> print a
 
 type Coordinates = (# Double | (# Double, Double #) #)
@@ -37,7 +37,7 @@ test_smallest = do
 
 main :: IO ()
 main = do
-  test_fib_next
-  test_fib_next'
-  test_sum_prod
+  test_fibNext
+  test_fibNext'
+  test_sumProd
   test_smallest
