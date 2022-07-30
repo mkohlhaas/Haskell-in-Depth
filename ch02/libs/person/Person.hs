64572c2429055bbe5a3adc46ae2338f960249701
Hans-J. Schmid <knock@myopendoor.de>
2022-07-30 06:12:42 +0200

    deletes timetable for good and always.

--- ch02/libs/person/Person.hs
+++ ch02/libs/person/Person.hs
@@ -8,6 +8,8 @@ type Age = Int
 type Name = String
 data Person = Person Name (Maybe Age)
 
+-- https://hackage.haskell.org/package/base-4.16.3.0/docs/Data-String.html#t:IsString
+-- Used by the overloaded string extension (-XOverloadedStrings in GHC).
 instance IsString Person where
   fromString name = Person name Nothing
 
