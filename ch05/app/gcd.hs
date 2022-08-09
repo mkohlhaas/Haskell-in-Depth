e4fcf5a5ad2485d3f40ad7cbae0263635b38f747
Hans-J. Schmid <knock@myopendoor.de>
2022-08-09 23:28:50 +0200

    cleanses more of ch. 5.

--- ch05/app/gcd.hs
+++ ch05/app/gcd.hs
@@ -1,18 +1,19 @@
 import Control.Monad.Writer (MonadWriter (tell), Sum (Sum), Writer, mapWriter)
 
 gcd' ∷ Integral a ⇒ a → a → a
-gcd' a 0 = a
-gcd' a b = gcd b (a `mod` b)
+gcd' x 0 = x
+gcd' x y = gcd y (x `mod` y)
 
 -- turn a pure computation into a monadic one ⇒ you can use Writer monad down the line
 gcdM ∷ (Integral a, Monad m) ⇒ (a → a → m ()) → a → a → m a
-gcdM step a 0 = step a 0 >> pure a
-gcdM step a b = step a b >> gcdM step b (a `mod` b)
+gcdM step x 0 = step x 0 >> pure x
+gcdM step x y = step x y >> gcdM step y (x `mod` y)
 
 gcdPrint ∷ (Show a, Integral a) ⇒ a → a → IO a
 gcdPrint = gcdM (curry print)
 
 -- gcdPrint = gcdM (\a b → print (a, b))
+-- `print` prints a tuple
 
 gcdCountSteps ∷ Integral a ⇒ a → a → Writer (Sum Int) a
 gcdCountSteps = gcdM (\_ _ → tell $ Sum 1)
